bfbc3d874ab939aabbbbaf119b47f8bbece4253d
Syed Mahbubuz Zaman <59284875+syedmahbubuzzaman@users.noreply.github.com>
2025-09-28 23:30:11 -0400

    front end

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from routes import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+# ✅ Allow frontend origin
+origins = [
+    "http://localhost:3000",   # React dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # origins allowed
+    allow_credentials=True,         # needed for cookies (refresh token)
+    allow_methods=["*"],            # allow all methods (POST, GET, etc.)
+    allow_headers=["*"],            # allow all headers (Authorization, etc.)
+)
+
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
